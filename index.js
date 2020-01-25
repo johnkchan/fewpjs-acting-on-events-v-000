7bf1a6c1c5fa5ead0ca37407ac192fcdeaf169a4
Jonathan Chan <johnkchan252@gmail.com>
2020-01-25 15:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(rightNumbers, 10);
  
   if (right < 360) {
     dodger.style.right = `${right + 1}px`;
